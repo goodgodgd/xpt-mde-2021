c65608aedd62811370caa812254f76e974dcbe8d
goodgodgd <goodgodgd@yonsei.ac.kr>
2020-08-15 02:54:35 +0900

    update config-example / remove legacy from convert_tfrecords_main.py

--- tfrecords/create_tfrecords_main.py
+++ tfrecords/create_tfrecords_main.py
@@ -1,30 +1,10 @@
-import os
 import os.path as op
-from glob import glob
 import numpy as np
 
 import settings
 from config import opts
 from utils.util_class import WrongInputException
-from utils.util_class import PathManager
-from tfrecords.tfrecord_writer import TfrecordMaker
 import tfrecords.tfrecord_maker as tm
-
-
-def convert_to_tfrecords():
-    src_paths = glob(op.join(opts.DATAPATH_SRC, "*"))
-    src_paths = [path for path in src_paths if op.isdir(path)]
-    print("[convert_to_tfrecords] top paths:", src_paths)
-    for srcpath in src_paths:
-        tfrpath = op.join(opts.DATAPATH_TFR, op.basename(srcpath))
-        if op.isdir(tfrpath):
-            print("[convert_to_tfrecords] tfrecord already created in", tfrpath)
-        else:
-            with PathManager([tfrpath]) as pm:
-                tfrmaker = TfrecordMaker(srcpath, tfrpath, opts.STEREO, opts.get_shape("SHWC"))
-                tfrmaker.make()
-                # if set_ok() was NOT excuted, the generated path is removed
-                pm.set_ok()
 
 
 def convert_to_tfrecords_directly():
