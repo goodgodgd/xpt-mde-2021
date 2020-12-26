fcb16695cae5298e708edbdd5e0139a9c1939ad3
goodgodgd <goodgodgd@yonsei.ac.kr>
2020-12-27 01:08:38 +0900

    update evaluation to save pose evaluation result in both absolute and relative scale and save evalutation summary file in csv

--- evaluate/evaluate_main.py
+++ evaluate/evaluate_main.py
@@ -1,50 +1,82 @@
 import os
 import os.path as op
 import numpy as np
+import pandas as pd
 
 import settings
 from config import opts
 import evaluate.eval_funcs as ef
+import utils.util_funcs as uf
 
 
 def evaluate_by_plan():
     for dataset_name, save_keys in opts.TEST_PLAN:
-        evaluate(dataset_name)
+        evaluate_dataset(dataset_name)
 
 
-def evaluate(dataset_name, pred_path=opts.DATAPATH_PRD, ckpt_name=opts.CKPT_NAME):
-    filename = op.join(pred_path, ckpt_name, dataset_name + ".npz")
+def evaluate_dataset(dataset_name, ckpt_name=opts.CKPT_NAME):
+    filename = op.join(opts.DATAPATH_PRD, ckpt_name, dataset_name + ".npz")
     results = np.load(filename)
     results = {key: results[key] for key in results.files}
     os.makedirs(op.join(opts.DATAPATH_EVL, ckpt_name), exist_ok=True)
 
     if "pose" in results and "pose_gt" in results:
-        trj_errors = []
-        rot_errors = []
-        for pose_pred, pose_true in zip(results["pose"], results["pose_gt"]):
-            print("pose shape", pose_pred.shape, pose_true.shape)
-            trj_err, rot_err = evaluate_pose(pose_pred, pose_true)
-            trj_errors.append(trj_err)
-            rot_errors.append(rot_err)
-
-        print("")
-        trj_errors = np.stack(trj_errors, axis=0)
-        rot_errors = np.stack(rot_errors, axis=0)
-        print(f"trajectory errors: {trj_errors.shape}\n{trj_errors[:5]}\n-> mean={np.mean(trj_errors, axis=0)}")
-        print(f"rotational errors: {rot_errors.shape}\n{rot_errors[:5]}\n-> mean={np.mean(rot_errors, axis=0)}")
-        os.makedirs(op.join(opts.DATAPATH_EVL, ckpt_name), exist_ok=True)
-        np.savetxt(op.join(opts.DATAPATH_EVL, ckpt_name, "trajectory_error.txt"), trj_errors, fmt="%1.4f")
-        np.savetxt(op.join(opts.DATAPATH_EVL, ckpt_name, "rotation_error.txt"), rot_errors, fmt="%1.4f")
+        trj_abs, rot_abs = evaluate_dataset_pose(results, True)
+        trj_rel, rot_rel = evaluate_dataset_pose(results, False)
+        save_pose_metric(ckpt_name, dataset_name, trj_abs, rot_abs, trj_rel, rot_rel)
 
     if "depth" in results and "depth_gt" in results:
-        depth_errors = []
-        for depth_pred, depth_true in zip(results["depth"], results["depth_gt"]):
-            depth_err = evaluate_depth(depth_pred, depth_true)
-            depth_errors.append(depth_err)
+        evaluate_dataset_depth(results, ckpt_name, dataset_name)
 
-        depth_errors = np.array(depth_errors)
-        print(f"depth errors: {depth_errors.shape}\n{depth_errors[:5]}\n-> mean={np.mean(depth_errors, axis=0)}")
-        np.savetxt(op.join(opts.DATAPATH_EVL, ckpt_name, "depth_error.txt"), depth_errors, fmt="%1.4f")
+
+def evaluate_dataset_pose(results, abs_scale):
+    num_samples = results["pose"].shape[0]
+    trj_errors = np.zeros((num_samples, 4), dtype=float)
+    rot_errors = np.zeros((num_samples, 4), dtype=float)
+    for i, (pose_pred, pose_true) in enumerate(zip(results["pose"], results["pose_gt"])):
+        trj_err, rot_err = evaluate_pose(pose_pred, pose_true, abs_scale)
+        trj_errors[i, :] = trj_err[1:]
+        rot_errors[i, :] = rot_err[1:]
+        uf.print_progress_status(f"{i} / {num_samples}")
+
+    print("")
+    absrel = "absolute" if abs_scale else "relative"
+    print(f"trajectory errors in {absrel} scale: {trj_errors.shape}")
+    print(f"-> mean={np.mean(trj_errors, axis=0)}, total mean={np.mean(trj_errors):1.5f}")
+    print(f"rotational errors: {rot_errors.shape}")
+    print(f"-> mean={np.mean(rot_errors, axis=0)}, total mean={np.mean(rot_errors):1.5f}")
+    return trj_errors, rot_errors
+
+
+def save_pose_metric(ckpt_name, dataset_name, trj_abs, rot_abs, trj_rel, rot_rel):
+    dstpath = op.join(opts.DATAPATH_EVL, ckpt_name)
+    os.makedirs(dstpath, exist_ok=True)
+    trj_errors = np.concatenate([trj_abs, trj_rel], axis=1)
+    rot_errors = np.concatenate([rot_abs, rot_rel], axis=1)
+    np.savetxt(op.join(dstpath, f"{dataset_name}_trjerr.txt"), trj_errors, fmt="%1.5f")
+    np.savetxt(op.join(dstpath, f"{dataset_name}_roterr.txt"), rot_errors, fmt="%1.5f")
+    results = {"trjmean_abs": [np.mean(trj_abs)], "trjstd_abs": [np.std(trj_abs)],
+               "trjmean_rel": [np.mean(trj_rel)], "trjstd_rel": [np.std(trj_rel)],
+               "rotmean_abs": [np.mean(rot_abs)], "rotstd_abs": [np.std(rot_abs)],
+               "rotmean_rel": [np.mean(rot_rel)], "rotstd_rel": [np.std(rot_rel)],}
+    results = pd.DataFrame(results)
+    print("pose eval result:\n", results)
+    results.to_csv(op.join(dstpath, f"{dataset_name}_pose_eval.csv"), index=False, float_format='%1.5f')
+
+
+def evaluate_dataset_depth(results, ckpt_name, dataset_name):
+    depth_errors = []
+    for depth_pred, depth_true in zip(results["depth"], results["depth_gt"]):
+        depth_err = evaluate_depth(depth_pred, depth_true)
+        depth_errors.append(depth_err)
+
+    depth_errors = np.array(depth_errors)
+    print(f"depth errors: {depth_errors.shape}\n{depth_errors[:5]}\n-> mean={np.mean(depth_errors, axis=0)}")
+    dstpath = op.join(opts.DATAPATH_EVL, ckpt_name)
+    np.savetxt(op.join(dstpath, f"{dataset_name}_depth.txt"), depth_errors, fmt="%1.4f")
+    results = pd.DataFrame(np.mean(depth_errors, axis=0)[np.newaxis, ...], columns=["abs_rel", "sq_rel", "rmse", "rmse_log", "a1", "a2", "a3"])
+    print("depth eval result:\n", results)
+    results.to_csv(op.join(dstpath, f"{dataset_name}_depth_eval.csv"), index=False, float_format='%1.5f')
 
 
 def evaluate_depth(depth_pred, depth_true):
@@ -66,19 +98,20 @@ def evaluate_depth(depth_pred, depth_true):
     return metrics
 
 
-def evaluate_pose(pose_pred, pose_true):
+def evaluate_pose(pose_pred, pose_true, abs_scale):
     """
     :param pose_pred: predicted source poses that transforms points in target to source frame
                     format=(tx, ty, tz, ux, uy, uz) shape=[numsrc, 6]
     :param pose_true: ground truth source poses that transforms points in target to source frame
                     format=(4x4 transformation), shape=[numsrc, 4, 4]
+    :param abs_scale: evaluate pose in absolute scale
     """
     # convert source and target poses to relative poses w.r.t first source pose
     # in 4x4 transformation matrix form
     pose_pred_mat = ef.recover_pred_snippet_poses(pose_pred)
     pose_true_mat = ef.recover_true_snippet_poses(pose_true)
 
-    trj_error = ef.calc_trajectory_error(pose_pred_mat, pose_true_mat)
+    trj_error = ef.calc_trajectory_error(pose_pred_mat, pose_true_mat, abs_scale)
     rot_error = ef.calc_rotational_error(pose_pred_mat, pose_true_mat)
     return trj_error, rot_error
 
